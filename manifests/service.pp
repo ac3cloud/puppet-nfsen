6d8626ca1d5999b91e8b2a2d58ff77757fa7eea0
Michael Baker <michael.baker@bulletproof.net>
2018-03-28 06:43:04 +0800

    More things

--- manifests/service.pp
+++ manifests/service.pp
@@ -6,6 +6,13 @@ class nfsen::service {
 
   assert_private()
 
+  file{'/lib/systemd/system/nfsen.service':
+    mode   => '0744',
+    owner  =>  'root',
+    group  => 'root',
+    source => 'puppet:///modules/nfsen/lib/systemd/system/nfsen.service'
+  } ->
+
   service { 'nfsen':
     ensure    => 'running',
     hasstatus => false,
